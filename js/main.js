4355faa70d1e188e8dd8c041acc9c61730c97ad8
ZinchAndrey <44947620+ZinchAndrey@users.noreply.github.com>
2020-10-11 23:51:00 +0300

    Добавляет модуль load.js для получения данных с сервера. Добавляет рендер картинок на основе этих данных

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
+(function () {
+  const URL = `https://21.javascript.pages.academy/kekstagram/data`;
+  function onError(message) {
+    console.error(message);
+  }
 
+  function onSuccess(data) {
+    // console.log(data);
+    window.picture.renderAllPictures(data);
+  }
+  window.load(URL, onSuccess, onError);
+
+
+})();
