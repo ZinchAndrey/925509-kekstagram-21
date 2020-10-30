31025033c8de8d77badc8f31fb3ea4e85f975833
ZinchAndrey <44947620+ZinchAndrey@users.noreply.github.com>
2020-10-30 21:32:12 +0300

    Добавляет активация кнопки при переключении фильтров

--- js/filter.js
+++ js/filter.js
@@ -9,7 +9,13 @@
     discussed: filterNode.querySelector(`#filter-discussed`),
   };
 
-  function filterDiscussedPictures() {
+  function activateButton(evt) {
+    const activeButtonNode = filterNode.querySelector(`.img-filters__button--active`);
+    activeButtonNode.classList.remove(`img-filters__button--active`);
+    evt.target.classList.add(`img-filters__button--active`);
+  }
+
+  function filterDiscussedPictures(evt) {
     // копируем исходный массив
     const pictures = window.pictures.slice();
     const discussedPictures = pictures.sort(function (a, b) {
@@ -17,19 +23,22 @@
     });
     window.picture.removeAllPictures();
     window.picture.renderAllPictures(discussedPictures);
+    activateButton(evt);
   }
 
-  function filterRandomPictures() {
+  function filterRandomPictures(evt) {
     const pictures = window.pictures.slice();
     const randomPictures = window.utils.shuffleArray(pictures).slice(0, RANDOM_PICTURES_QUANTITY);
     window.picture.removeAllPictures();
     window.picture.renderAllPictures(randomPictures);
+    activateButton(evt);
   }
 
-  function filterDefaultPictures() {
+  function filterDefaultPictures(evt) {
     const pictures = window.pictures.slice();
     window.picture.removeAllPictures();
     window.picture.renderAllPictures(pictures);
+    activateButton(evt);
   }
 
   FilterButton.discussed.addEventListener(`click`, filterDiscussedPictures);
